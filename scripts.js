686894c477424e1cf8c7a5dd144bdb9a897c58f5
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-08-21 09:52:46 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/"}
+const engine = "duckduckgo"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/",
+  duckduckgo: "https://duckduckgo.com/?q=",
+  ecosia: "https://www.ecosia.org/search?q=",
+  google: "https://www.google.com/search?q=",
+  startpage: "https://www.startpage.com/search?q=",
+  youtube: "https://www.youtube.com/results?q=",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  return engineUrls[engine] + value
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"3isK5XQD2elr2V1E","label":"My URL","bookmarks":[{"id":"j721s0WcX9FyZtZQ","label":"GitHub","url":"https://github.com/d0UBleW"},{"id":"6pJvPUoMwn1e5fkf","label":"LinkedIn","url":"https://linkedin.com/in/williamwijaya02"}]},{"id":"gVPzArI7beu4P21B","label":"Media","bookmarks":[{"id":"CziOrkoZMmRi9oE0","label":"YouTube","url":"https://youtube.com"},{"id":"QLeizioQxytT09dk","label":"singsing","url":"https://twitch.tv/singsing"}]},{"id":"oyNGa583Mbsl5Yql","label":"IDK","bookmarks":[{"id":"ntOrbg9mwA7dHjiY","label":"HackTricks","url":"https://book.hacktricks.xyz/"}]},{"id":"63DkPlr3ywPbkQqo","label":"APU","bookmarks":[{"id":"3dhpD7g259m85Yia","label":"APSPACE","url":"https://apspace.apu.edu.my"}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
